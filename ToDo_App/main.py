02d195c570b4fb67dc956c78bc4d18d3a1772fd7
Krishna Prasad Guntur <krishnaprasad.guntur@gmail.com>
2026-06-19 13:28:32 +0530

    Added API endpoints for todo

--- ToDo_App/main.py
+++ ToDo_App/main.py
@@ -1,8 +1,46 @@
-from fastapi import FastAPI
-from database import engine
+from typing import Annotated
+
+from fastapi import FastAPI, Depends, HTTPException, Path
+from sqlalchemy.orm import Session
+from starlette import status
+
+from database import engine, local_session
 import models
+from models import TODOS
+from todoRequest import TodoRequest
 
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
+
+def get_db():
+    db = local_session()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.get("/get_todos")
+def get_all_todos(db: db_dependency):
+    return db.query(TODOS).all()
+
+@app.get("/get_todos/{todo_id}", status_code=status.HTTP_200_OK)
+async def get_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(TODOS).filter(TODOS.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise  HTTPException(
+        status_code=404,
+        detail=f"Record with ID {todo_id} not found"
+    )
+
+@app.post("/create_todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependency, new_todo: TodoRequest):
+    todo_model = TODOS(
+        **new_todo.model_dump()
+    )
+    db.add(todo_model)
+    db.commit()
