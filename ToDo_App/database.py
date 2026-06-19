28525210338912b3581c36a819901ef7dfb0b0fb
Krishna Prasad Guntur <krishnaprasad.guntur@gmail.com>
2026-06-19 12:18:37 +0530

    Added ToDo App Package

---
+++ ToDo_App/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:090909@localhost/ToDo_App_DB'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+local_session = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+
+Base = declarative_base()
