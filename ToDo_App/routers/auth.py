bbb54c2ea81af73dfa9bde6c97f56875f197b099
krishna-guntur <gvns.krishna1@gmail.com>
2026-06-20 18:58:02 +0530

    Added Auth Code

--- ToDo_App/routers/auth.py
+++ ToDo_App/routers/auth.py
@@ -54,4 +54,4 @@ async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         return 'Failed authentication'
-    return 'Successful authentication'
+    return 'Successful  authentication'
